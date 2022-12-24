3e08516aae63d521761d99782500e7bc9e43764a
pawitsahare0 <119015232+pawitsahare0@users.noreply.github.com>
2022-12-24 12:57:36 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Your Welcome " />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This website is host by sinagpur 10gbps , internet  <code>pages/index.js</code>
         </p>
       </main>
 
